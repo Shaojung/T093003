f4619f95c663e32c2fdd32e8703aa218ae2a2198
Shaojung <abc@abc.com>
2016-09-30 21:35:28 +0800

    Add SharedPreferences.

--- app/src/main/java/tw/com/pcschool/t093003/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t093003/MainActivity.java
@@ -3,6 +3,9 @@ package tw.com.pcschool.t093003;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+    public void clickRead(View v)
+    {
+        SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
+        TextView tv = (TextView)findViewById(R.id.textView);
+        String str = sp.getString("name", "");
+        tv.setText(str);
+    }
+    public void clickWrite(View v)
+    {
         SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
         SharedPreferences.Editor ed = sp.edit();
-        ed.putString("name", "Teacher");
+        EditText editText = (EditText) findViewById(R.id.editText);
+        ed.putString("name", editText.getText().toString());
         ed.commit();
     }
 }
